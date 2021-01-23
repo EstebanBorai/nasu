4b1528307d178f16035707df1975410b77c77352
Esteban Borai <estebanborai@gmail.com>
2021-01-23 14:26:33 -0300

    dev(test): impl unit test sample for perform_http
    Implements Unit Testing examples and provide ergonomics for asynchronous
    functions tests

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,11 +1,52 @@
+mod macros;
+
 pub fn hello() {
   println!("Welcome to Nasu! 🧑🏻‍⚕️ Command-line utility which poll on remote addresses in order to perform status checks periodically");
 }
 
+/// HTTP Request parameters to provided to the
+/// `perform_http` function.
+#[derive(Debug)]
+pub struct HttpTarget {
+  pub url: String,
+  pub method: String,
+}
+
+/// Details gathered from the `HttpTarget` performance
+#[derive(Debug, PartialEq)]
+pub struct HttpResult {
+  pub(crate) status_code: u16,
+  pub(crate) body: Option<Vec<u8>>,
+}
+
+pub async fn perform_http(_: &HttpTarget) -> HttpResult {
+  // A HTTP Request must be performed in order to
+  // built a `HttpResult` struct.
+  //
+  // The current value is provided as an example of
+  // the expected response
+  HttpResult {
+    status_code: 200_u16,
+    body: None,
+  }
+}
+
 #[cfg(test)]
 mod tests {
+  use super::*;
+
   #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
+  fn it_retrieves_http_response() {
+    let http_target = HttpTarget {
+      url: String::from("https://httpbin.org/status/200"),
+      method: String::from("GET"),
+    };
+
+    let http_result = HttpResult {
+      status_code: 200 as u16,
+      body: None,
+    };
+
+    assert_eq!(bo!(perform_http(&http_target)), http_result);
   }
 }
